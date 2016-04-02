adb4ded8f90686e72df50750b76c230127e94219
AleDanish <superalle87@gmail.com>
2016-04-02 20:51:07 +0000

    db conf

--- database_config.sh
+++ database_config.sh
@@ -9,22 +9,24 @@ echo $oldfloatingip
 
 #compress data
 Tcompress_start=$(date +%s%3N)
-ssh -y -i /home/ubuntu/key/mcn-key.pem ubuntu@$oldfloatingip 'cd /var/lib/influxdb; sudo tar -zcvf /home/ubuntu/influxdb.backup.tar.gz data hh wal meta'
+ssh -y -i /home/ubuntu/key/mcn-key.pem ubuntu@$oldfloatingip 'cd /var/lib/influxdb; sudo tar -cvf /home/ubuntu/influxdb.backup.tar data hh wal'
 Tcompress_end=$(date +%s%3N)
 #send data
 Tmove_start=$(date +%s%3N)
-scp -oStrictHostKeyChecking=no -i /home/ubuntu/key/mcn-key.pem ubuntu@$oldfloatingip:/home/ubuntu/influxdb.backup.tar.gz /home/ubuntu/
+scp -oStrictHostKeyChecking=no -i /home/ubuntu/key/mcn-key.pem ubuntu@$oldfloatingip:/home/ubuntu/influxdb.backup.tar /home/ubuntu/
 Tmove_end=$(date +%s%3N)
 #remove data folders
-rm -rf /var/lib/influxdb/*
+echo "cancello i dati"
+sudo rm -rf /var/lib/influxdb/*
 #extract data
 Textract_start=$(date +%s%3N)
-cd /home/ubuntu
-tar -zxvf influxdb.backup.tar.gz -C  /var/lib/influxdb
+echo "estraggo i dati"
+sudo tar -xf /home/ubuntu/influxdb.backup.tar -C /var/lib/influxdb
 Textract_end=$(date +%s%3N)
 #restart database
 Trestart_start=$(date +%s%3N)
-service influxdb restart
+echo "database restart"
+sudo service influxdb restart
 Trestart_end=$(date +%s%3N)
 Tend=$(date +%s%3N)
 
@@ -34,7 +36,8 @@ Textract=$(((Textract_end-Textract_start)/1000))
 Trestart=$(((Trestart_end-Trestart_start)/1000))
 Ttotal=$(((Tend-Tstart)/1000))
 
-echo "Time to compress data: " $Tcompress >> /home/ubuntu/times_influxdb
-echo "Time to move data: " $Tmove >> /home/ubuntu/times_influxdb
-echo "Time to extract data: " $Textract >> /home/ubuntu/times_influxdb
-echo "Time total: " $Ttotal >> /home/ubuntu/times_influxdb
+sudo rm /home/ubuntu/times_influxdb
+sudo echo "Time to compress data: " $Tcompress >> /home/ubuntu/times_influxdb
+sudo echo "Time to move data: " $Tmove >> /home/ubuntu/times_influxdb
+sudo echo "Time to extract data: " $Textract >> /home/ubuntu/times_influxdb
+sudo echo "Time total: " $Ttotal >> /home/ubuntu/times_influxdb
